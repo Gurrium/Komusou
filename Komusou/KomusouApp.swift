8ba0fccd6939fb8af35449cfdaa6417e10fc54d1
gurrium <gurrium@hatena.ne.jp>
2022-03-05 19:56:53 +0900

    Initial Commit

---
+++ Komusou/KomusouApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomusouApp.swift
+//  Komusou
+//
+//  Created by gurrium on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct KomusouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
