2402370b078ff33e2dd04db704b827a5539447a0
gurrium <gurrium@hatena.ne.jp>
2022-03-05 20:07:07 +0900

    必要そうなファイルを作った

--- Komusou/KomusouApp.swift
+++ Komusou/KomusouApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KomusouApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PlayView()
         }
     }
 }
--- Komusou/PlayView.swift
+++ Komusou/PlayView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  PlayView.swift
 //  Komusou
 //
 //  Created by gurrium on 2022/03/05.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PlayView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct PlayView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        PlayView()
     }
 }
