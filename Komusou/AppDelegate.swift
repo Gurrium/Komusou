3a3183efd6b2628f295d746794b420fb75fc25f2
gurrium <gurrium@hatena.ne.jp>
2022-05-13 19:51:11 +0900

    DEBUG環境ではCentralManagerMockを使うようにした

--- Komusou/AppDelegate.swift
+++ Komusou/AppDelegate.swift
@@ -11,7 +11,19 @@ import UIKit
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        BluetoothManager.setUp(centralManager: CBCentralManager())
+        let centralManager: CentralManager
+        #if DEBUG
+            centralManager = CentralManagerMock()
+        #else
+            centralManager = CBCentralManager()
+        #endif
+
+        BluetoothManager.setUp(centralManager: centralManager)
+
+        #if DEBUG
+            (centralManager as! CentralManagerMock).state = .poweredOn
+            BluetoothManager.shared().centralManagerDidUpdateState(centralManager)
+        #endif
 
         return true
     }
