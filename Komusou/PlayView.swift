3baabc37fa84777c5c4189551b02b06ef4f7c680
gurrium <gurrium@hatena.ne.jp>
2022-05-16 21:43:53 +0900

    Bluetoothが有効かのロジックをKomusouAppに移した

--- Komusou/PlayView.swift
+++ Komusou/PlayView.swift
@@ -10,40 +10,23 @@ import SwiftUI
 struct PlayView: View {
     @State
     var isSettingsPresented = false
-    @State
-    private var isBluetoothEnabled = BluetoothManager.shared().isBluetoothEnabled
 
     var body: some View {
-        Group {
-            if isBluetoothEnabled {
-                ZStack(alignment: .topTrailing) {
-                    WorldView(speedSensor: KomusouApp.speedSensor, cadenceSensor: KomusouApp.cadenceSensor)
-                        .edgesIgnoringSafeArea(.all)
-                    Button {
-                        isSettingsPresented = true
-                    } label: {
-                        Image(systemName: "gearshape")
-                            .resizable()
-                            .padding(8)
-                            .frame(width: 44, height: 44)
-                            .foregroundColor(.black)
-                    }
-                }
-
-                .sheet(isPresented: $isSettingsPresented) {
-                    SettingsView()
-                }
-            } else {
-                VStack(spacing: 16) {
-                    Text("Bluetoothを有効にしてください")
-                    Button("設定画面を開く") {
-                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                    }
-                }
+        ZStack(alignment: .topTrailing) {
+            WorldView(speedSensor: KomusouApp.speedSensor, cadenceSensor: KomusouApp.cadenceSensor)
+                .edgesIgnoringSafeArea(.all)
+            Button {
+                isSettingsPresented = true
+            } label: {
+                Image(systemName: "gearshape")
+                    .resizable()
+                    .padding(8)
+                    .frame(width: 44, height: 44)
+                    .foregroundColor(.black)
             }
         }
-        .onReceive(BluetoothManager.shared().$isBluetoothEnabled) { isBluetoothEnabled in
-            self.isBluetoothEnabled = isBluetoothEnabled
+        .sheet(isPresented: $isSettingsPresented) {
+            SettingsView()
         }
     }
 }
