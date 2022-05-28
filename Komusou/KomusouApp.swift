3f1930afc9ce7636e98cee56403eea4f08cb8c5f
gurrium <gurrium@hatena.ne.jp>
2022-05-28 22:29:03 +0900

    センサーの渡し方を変えた

--- Komusou/KomusouApp.swift
+++ Komusou/KomusouApp.swift
@@ -14,12 +14,48 @@ import SwiftUI
 
 @main
 struct KomusouApp: App {
+    static let bluetoothSpeedSensor = BluetoothSpeedSensor()
+    static let mockSpeedSensor = MockSpeedSensor()
+
     @UIApplicationDelegateAdaptor
     private var appDelegate: AppDelegate
+    @State
+    private var isSettingsPresented = false
+    @State
+    private var isBluetoothEnabled = true
 
     var body: some Scene {
         WindowGroup {
-            PlayView()
+            ZStack {
+                ZStack(alignment: .topTrailing) {
+                    AltWorldView(speedSensor: isBluetoothEnabled ? Self.bluetoothSpeedSensor : Self.mockSpeedSensor)
+                    Button {
+                        isSettingsPresented = true
+                    } label: {
+                        Image(systemName: "gearshape")
+                            .resizable()
+                            .padding(8)
+                            .frame(width: 44, height: 44)
+                            .foregroundColor(.black)
+                    }
+                }
+                .sheet(isPresented: $isSettingsPresented) {
+                    SettingsView()
+                }
+                if !isBluetoothEnabled {
+                    VStack(spacing: 8) {
+                        Text("デモを表示しています")
+                        Text("Bluetoothを有効にしてください")
+                        Button("設定画面を開く") {
+                            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.gray.opacity(0.5)) // TODO: 背景色と文字色を修正する
+                }
+            }.onReceive(BluetoothManager.shared().$isBluetoothEnabled) { isBluetoothEnabled in
+                self.isBluetoothEnabled = isBluetoothEnabled
+            }
         }
     }
 }
--- Komusou/PlayView.swift
+++ Komusou/PlayView.swift
@@ -1,60 +1,57 @@
+////
+////  PlayView.swift
+////  Komusou
+////
+////  Created by gurrium on 2022/03/05.
+////
 //
-//  PlayView.swift
-//  Komusou
+// import SwiftUI
 //
-//  Created by gurrium on 2022/03/05.
+// struct PlayView: View {
+//    static let bluetoothSpeedSensor = BluetoothSpeedSensor()
+//    static let mockSpeedSensor = MockSpeedSensor()
 //
-
-import SwiftUI
-
-struct PlayView: View {
-    @State
-    var isSettingsPresented = false
-    @State
-    private var isBluetoothEnabled = true
-
-    var body: some View {
-        ZStack {
-            ZStack(alignment: .topTrailing) {
-                // FIXME: センサーをここで初期README化してるのよくないきがする。もっと高きところにあるのが正しそう
-//                WorldView(
-//                    speedSensor: isBluetoothEnabled ? BluetoothSpeedSensor() : MockSpeedSensor()// ,
-//                    cadenceSensor: isBluetoothEnabled ? BluetoothCadenceSensor() : MockCadenceSensor()
-//                )
-//                .edgesIgnoringSafeArea(.all)
-                AltWorldView(speedSensor: MockSpeedSensor())
-                Button {
-                    isSettingsPresented = true
-                } label: {
-                    Image(systemName: "gearshape")
-                        .resizable()
-                        .padding(8)
-                        .frame(width: 44, height: 44)
-                        .foregroundColor(.black)
-                }
-            }
-            .sheet(isPresented: $isSettingsPresented) {
-                SettingsView()
-            }
-            if !isBluetoothEnabled {
-                VStack(spacing: 8) {
-                    Text("デモを表示しています")
-                    Text("Bluetoothを有効にしてください")
-                    Button("設定画面を開く") {
-                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                    }
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(.gray.opacity(0.5)) // TODO: 背景色と文字色を修正する
-            }
-        }.onReceive(BluetoothManager.shared().$isBluetoothEnabled) { isBluetoothEnabled in
-            self.isBluetoothEnabled = isBluetoothEnabled
-        }
-    }
-}
-
-struct PlayView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlayView()
-    }
-}
+//    @State
+//    var isSettingsPresented = false
+//    @State
+//    private var isBluetoothEnabled = true
+//
+//    var body: some View {
+//        ZStack {
+//            ZStack(alignment: .topTrailing) {
+//                AltWorldView(speedSensor: isBluetoothEnabled ? Self.bluetoothSpeedSensor : Self.mockSpeedSensor)
+//                Button {
+//                    isSettingsPresented = true
+//                } label: {
+//                    Image(systemName: "gearshape")
+//                        .resizable()
+//                        .padding(8)
+//                        .frame(width: 44, height: 44)
+//                        .foregroundColor(.black)
+//                }
+//            }
+//            .sheet(isPresented: $isSettingsPresented) {
+//                SettingsView()
+//            }
+//            if !isBluetoothEnabled {
+//                VStack(spacing: 8) {
+//                    Text("デモを表示しています")
+//                    Text("Bluetoothを有効にしてください")
+//                    Button("設定画面を開く") {
+//                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+//                    }
+//                }
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .background(.gray.opacity(0.5)) // TODO: 背景色と文字色を修正する
+//            }
+//        }.onReceive(BluetoothManager.shared().$isBluetoothEnabled) { isBluetoothEnabled in
+//            self.isBluetoothEnabled = isBluetoothEnabled
+//        }
+//    }
+// }
+//
+// struct PlayView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PlayView()
+//    }
+// }
